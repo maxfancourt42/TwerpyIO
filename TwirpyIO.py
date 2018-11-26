f01873c1e7959f372cd8417d2a29be788d202bb8
Max <max.fancourt@gmail.com>
2018-11-26 17:03:44 +0000

    Update of IO not yet functional control panel

--- TwirpyIO.py
+++ TwirpyIO.py
@@ -4,9 +4,9 @@ from tkinter import ttk
 
 class MainApplication:
     def __init__(self, master):
-
         # create the local root
         self.master = master
+        self.master.state('zoomed')
 
         # define the GUI name
         master.title("Twirpy IO")
@@ -16,51 +16,134 @@ class MainApplication:
         style.configure("TFrame", background="#ff9232")
 
         # create the mainframe
-        self.mainframe = tk.Frame(root, background="#FFFFFF")
+        self.mainframe = tk.Frame(master, background="#FFFFFF")
 
         # set mainframe parameteres
         self.mainframe.grid(column=0, row=0, sticky=tk.NSEW)
         self.mainframe.master.minsize(width=510, height=510)
 
-        self.label = tk.Label(self.mainframe, text="Twirpy IO", background="#ffc532", font=(None, 15))
-        self.label.grid(row=0, column=0, sticky="NSEW", columnspan=4, rowspan=1)
+        self.label = tk.Label(self.mainframe, text="Twirpy IO", background="#ffc532", font=(None, 15), anchor="w")
+        self.label.grid(row=0, column=1, sticky=tk.NSEW, columnspan=3)
 
         self.logo = tk.Label(self.mainframe, text="Logo", background="#ffc532", font=(None, 15))
-        self.logo.grid(row=0, column=0, sticky="NSW", rowspan=1)
+        self.logo.grid(row=0, column=3, sticky=tk.NSEW)
 
-        self.systemstatus = tk.Label(self.mainframe, text="Status Bar", background="#ff5e32", font=(None, 15))
-        self.systemstatus.grid(row=1, column=0, rowspan=6, sticky=tk.NSEW)
+        self.controlpaneltext = tk.Label(self.mainframe, text="Control Panel v1.0", background="#636363", font=(None, 15))
+        self.controlpaneltext.grid(row=0, column=0, sticky=tk.NSEW)
 
-        self.power = tk.Label(self.mainframe, text="Power", background="#f70000", font=(None, 12))
-        self.power.grid(row=2, column=0)
+        # Create a frame to hold the control panel on the left hand side
+        self.controlpanel = tk.Frame(self.mainframe, background="#636363")
+        self.controlpanel.grid(row=1, column=0, sticky=tk.NSEW, rowspan=4)
+        self.controlpanel.grid_propagate(False)
 
-        self.Controls = tk.Label(self.mainframe, text="Controls", background="#f70000", font=(None, 12))
-        self.Controls.grid(row=3, column=0)
+        # within this create a canvas that will scroll
+        self.canvas = tk.Canvas(self.controlpanel, background="#636363")
+        self.canvas.config(background="#636363", highlightthickness=0)
+        self.canvas.grid(row=0, column=0, sticky=tk.NSEW)
 
-        self.Siren = tk.Label(self.mainframe, text="Siren", background="#f70000", font=(None, 12))
-        self.Siren.grid(row=4, column=0)
+        # create a frame within the canvas to structure
+        #self.canvasframe = tk.Frame(self.canvas, background="#636363")
+        self.canvasframe = tk.Frame(self.canvas, background="#ffffff")
+        self.canvasframe.grid(row=0, column=0, sticky=tk.EW)
 
-        self.greet_button = tk.Button(self.mainframe, text="Power On", command=self.greet, background="#f70000")
-        self.greet_button.grid(row=4, column=1, columnspan=3)
+        # add the control panel buttons to the canvas
+        self.power = tk.Label(self.canvasframe, text="Power", background="#7a7a7a", font=(None, 12))
+        self.power.grid(row=0, column=0, sticky=tk.EW)
 
-        self.close_button = tk.Button(self.mainframe, text="Quit Program", command=master.quit, background="#ff5e32")
-        self.close_button.grid(row=6, column=3, sticky=tk.SE)
+        self.Controls = tk.Label(self.canvasframe, text="Controls", background="#7a7a7a", font=(None, 12))
+        self.Controls.grid(row=1, column=0, sticky=tk.EW)
+
+        self.Siren = tk.Label(self.canvasframe, text="Siren", background="#7a7a7a", font=(None, 12))
+        self.Siren.grid(row=2, column=0, sticky=tk.EW)
+
+        self.greet_button = tk.Button(self.canvasframe, text="Power On", command=self.greet, background="#636363")
+        self.greet_button.grid(row=3, column=0, sticky=tk.EW)
+
+        self.close_button = tk.Button(self.canvasframe, text="Quit Program", command=master.quit, background="#ff5e32")
+        self.close_button.grid(row=4, column=0, sticky=tk.EW)
+
+        # add a scroll bar to the canvvas
+        self.vsb = ttk.Scrollbar(self.controlpanel, orient="vertical", command=self.canvas.yview)
+        self.vsb.grid(row=0, column=1, sticky="NSE")
+        self.canvas.configure(yscrollcommand=self.vsb.set)
+
+        # create the canvas having filled it
+        self.canvas.create_window((0, 0), window=self.canvasframe, anchor=tk.NW)
+
+        # if the window is resized then adjust the bounding box of the canvas to prevent being able to scroll off screen
+        self.canvasframe.bind("<Configure>", self.OnFrameConfigure)
+        self.canvas.bind("<Configure>", self.OnCavasConfigure)
+
+        self.controlpanel.grid_columnconfigure(0, weight=1)
+        self.controlpanel.grid_rowconfigure(0, weight=1)
+
+        # create the grid of labels in the middle which represent the subsystems
+        self.powersubsystem = tk.Label(self.mainframe, text="Power", background="#7a7a7a")
+        self.powersubsystem.grid(row=1, column=1, sticky=tk.NSEW)
+        self.powersubsystem.grid_configure(padx=5, pady=5)
+
+        self.controlsubsystem = tk.Label(self.mainframe, text="Controls", background="#7a7a7a")
+        self.controlsubsystem.grid(row=1, column=2, sticky=tk.NSEW)
+        self.controlsubsystem.grid_configure(padx=5, pady=5)
+
+        self.sirensubsystem = tk.Label(self.mainframe, text="Siren", background="#7a7a7a")
+        self.sirensubsystem.grid(row=1, column=3, sticky=tk.NSEW)
+        self.sirensubsystem.grid_configure(padx=5, pady=5)
+
+        self.motorsubsystem = tk.Label(self.mainframe, text="Motor", background="#7a7a7a")
+        self.motorsubsystem.grid(row=2, column=1, sticky=tk.NSEW)
+        self.motorsubsystem.grid_configure(padx=5, pady=5)
+
+        self.AIsubsystem = tk.Label(self.mainframe, text="AI", background="#7a7a7a")
+        self.AIsubsystem.grid(row=2, column=2, sticky=tk.NSEW)
+        self.AIsubsystem.grid_configure(padx=5, pady=5)
+
+        self.bluetoothsubsystem = tk.Label(self.mainframe, text="Bluetooth", background="#7a7a7a")
+        self.bluetoothsubsystem.grid(row=2, column=3, sticky=tk.NSEW)
+        self.bluetoothsubsystem.grid_configure(padx=5, pady=5)
+
+        self.hoseintergrationsubsystem = tk.Label(self.mainframe, text="Water Hose", background="#7a7a7a")
+        self.hoseintergrationsubsystem.grid(row=3, column=1, sticky=tk.NSEW)
+        self.hoseintergrationsubsystem.grid_configure(padx=5, pady=5)
+
+        self.fluxcapacitorsubsystem = tk.Label(self.mainframe, text="Flux Capacitor", background="#7a7a7a")
+        self.fluxcapacitorsubsystem.grid(row=3, column=2, sticky=tk.NSEW)
+        self.fluxcapacitorsubsystem.grid_configure(padx=5, pady=5)
+
+        self.selfdestructsubsystem = tk.Label(self.mainframe, text="Self Destruct", background="#7a7a7a")
+        self.selfdestructsubsystem.grid(row=3, column=3, sticky=tk.NSEW)
+        self.selfdestructsubsystem.grid_configure(padx=5, pady=5)
 
         # give weight to root rows and column to permit all children to fill the frame
         self.master.columnconfigure(0, weight=1)
         self.master.rowconfigure(0, weight=1)
 
         # give weight to rows and column to permit dynamic redraw
-        self.mainframe.columnconfigure((0, 1, 2, 3), weight=1)
-        self.mainframe.rowconfigure((0, 1, 2, 3, 4, 5, 6), weight=1)
+        self.mainframe.columnconfigure(0, weight=1)
+        self.mainframe.columnconfigure((1, 2, 3), weight=2)
+        self.mainframe.rowconfigure((0, 1, 2, 3), weight=1)
 
         # Buff and pad all children of mainframe
         for child in self.mainframe.winfo_children():
             child.grid_configure(padx=5, pady=5)
 
+        # canvas frame pad
+        for child in self.canvasframe.winfo_children():
+            child.grid_configure(padx=5, pady=5)
+
     def greet(self):
         print("James Touched Me Inappropriately")
 
+    """Resize the bounding box of the control panel on dynamic resize"""
+    def OnFrameConfigure(self, event):
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+
+    def OnCavasConfigure(self, event):
+        width = 0
+        for child in self.canvasframe.grid_slaves():
+            width += child.winfo_reqwidth()
+
+        self.canvasframe.config(width=width, height=event.height)
 
 if __name__ == '__main__':
     root = tk.Tk()
