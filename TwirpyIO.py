4de4b96fb71ed7667d8f5681b3ffed7cb03b6e49
Max <max.fancourt@gmail.com>
2018-11-30 16:58:02 +0000

    Conversion of scrollbar to fixed, addition of logo, to be finished.

--- TwirpyIO.py
+++ TwirpyIO.py
@@ -1,12 +1,17 @@
 # import libaries
 import tkinter as tk
 from tkinter import ttk
+import os
+from PIL import Image
+from PIL import ImageTk
 
 class MainApplication:
     def __init__(self, master):
         # create the local root
         self.master = master
         self.master.state('zoomed')
+        self.master.columnconfigure(0, weight=1)
+        self.master.rowconfigure(0, weight=1)
 
         # define the GUI name
         master.title("Twirpy IO")
@@ -20,57 +25,46 @@ class MainApplication:
 
         # set mainframe parameteres
         self.mainframe.grid(column=0, row=0, sticky=tk.NSEW)
-        self.mainframe.master.minsize(width=510, height=510)
+        self.mainframe.master.minsize(width=600, height=510)
 
-        self.label = tk.Label(self.mainframe, text="Twirpy IO", background="#ffc532", font=(None, 15), anchor="w")
+        self.label = tk.Label(self.mainframe, text="Twirpy IO", background="#ffc532", font=(None, 18), anchor="w")
         self.label.grid(row=0, column=1, sticky=tk.NSEW, columnspan=3)
 
-        self.logo = tk.Label(self.mainframe, text="Logo", background="#ffc532", font=(None, 15))
-        self.logo.grid(row=0, column=3, sticky=tk.NSEW)
+        root.update()
+
+        # get logo from file
+        self.logoactualorignal = Image.open("%s\\logo.png" % os.path.dirname(__file__))
+        self.logoactual = self.logoactualorignal.resize((self.label.winfo_height(), self.label.winfo_height()), Image.ANTIALIAS)
+        self.photoimage = ImageTk.PhotoImage(self.logoactual)
+        self.logo = tk.Label(self.mainframe, image=self.photoimage, borderwidth=0)
+        self.logo.grid(row=0, column=2, columnspan=2, sticky=tk.E)
+        root.update_idletasks()
+
+        # on frame resize
+        self.mainframe.bind("<Configure>", self.resizeimage)
 
         self.controlpaneltext = tk.Label(self.mainframe, text="Control Panel v1.0", background="#636363", font=(None, 15))
         self.controlpaneltext.grid(row=0, column=0, sticky=tk.NSEW)
 
         # Create a frame to hold the control panel on the left hand side
         self.controlpanel = tk.Frame(self.mainframe, background="#636363")
-        self.controlpanel.grid(row=1, column=0, sticky=tk.NSEW, rowspan=4)
-        self.controlpanel.grid_rowconfigure(0, weight=1)
+        self.controlpanel.grid(row=1, column=0, sticky=tk.NSEW, rowspan=3)
+        self.controlpanel.grid_rowconfigure((0, 1, 2, 3, 4, 5), weight=1)
         self.controlpanel.grid_columnconfigure(0, weight=1)
 
-        # within this create a canvas that will scroll
-        self.canvas = tk.Canvas(self.controlpanel, background="#636363")
-        self.canvas.config(background="#636363", highlightthickness=0)
-        self.canvas.grid(row=0, column=0, sticky=tk.NSEW)
-
-        # add a scroll bar to the canvvas
-        self.vsb = ttk.Scrollbar(self.controlpanel, orient="vertical", command=self.canvas.yview)
-        self.vsb.grid(row=0, column=1, sticky="NSE")
-        self.canvas.configure(yscrollcommand=self.vsb.set)
-
-        # create a frame within the canvas to structure
-        #self.canvasframe = tk.Frame(self.canvas, background="#636363")
-        self.canvasframe = tk.Frame(self.canvas, background="#ffffff")
-        self.canvasframe.grid(row=0, column=0, sticky=tk.NSEW)
-
-        # create the canvas having filled it
-        self.canvas.create_window((0, 0), window=self.canvasframe, anchor=tk.NW)
-
-        # create a blank dictionary to track the buttons
-        numberofbuttons = 100
-        buttons = [[ttk.Button()] for x in range(numberofbuttons)]
-
-        # iterate over dictionary creating the required buttons
-
-        for i in range(numberofbuttons):
-            buttons[i] = ttk.Button(self.canvasframe, text="Temp text")
-            buttons[i].grid(row=i, column=0, sticky=tk.NSEW)
-
-        # set the scrolling region
-        self.canvas.config(scrollregion=self.canvas.bbox("all"))
-
-        # if the window is resized then adjust the bounding box of the canvas to prevent being able to scroll off screen
-        self.canvasframe.bind("<Configure>", self.OnFrameConfigure)
-        self.canvas.bind("<Configure>", self.FrameWidth)
+        # create the buttons on the left hand side
+        self.button1 = ttk.Button(self.controlpanel, text="Kill James")
+        self.button1.grid(row=0, column=0, sticky=tk.NSEW)
+        self.button2 = ttk.Button(self.controlpanel, text="Touch James")
+        self.button2.grid(row=1, column=0, sticky=tk.NSEW)
+        self.button3 = ttk.Button(self.controlpanel, text="Lick James")
+        self.button3.grid(row=2, column=0, sticky=tk.NSEW)
+        self.button4 = ttk.Button(self.controlpanel, text="Blame James")
+        self.button4.grid(row=3, column=0, sticky=tk.NSEW)
+        self.button5 = ttk.Button(self.controlpanel, text="Poke James")
+        self.button5.grid(row=4, column=0, sticky=tk.NSEW)
+        self.button6 = ttk.Button(self.controlpanel, text="Quit Program")
+        self.button6.grid(row=5, column=0, sticky=tk.NSEW)
 
         # create the grid of labels in the middle which represent the subsystems
         self.powersubsystem = tk.Label(self.mainframe, text="Power", background="#7a7a7a")
@@ -123,20 +117,27 @@ class MainApplication:
             child.grid_configure(padx=5, pady=5)
 
         # canvas frame pad
-        for child in self.canvasframe.winfo_children():
+        for child in self.controlpanel.winfo_children():
             child.grid_configure(padx=5, pady=5)
 
     def greet(self):
         print("James Touched Me Inappropriately")
 
     """Resize the bounding box of the control panel on dynamic resize"""
-    def OnFrameConfigure(self, event):
-        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+    def resizeimage(self, event):
+        # get height of top bar
+        self.height = self.label.winfo_height()
+        print(self.height)
 
-    def FrameWidth(self, event):
-        self.canvas.itemconfig(self.canvas, width=event.width)
+        if self.height > 200:
+            self.logoactual = self.logoactualorignal.resize((200, 200), Image.ANTIALIAS)
+        else:
+            self.logoactual = self.logoactualorignal.resize((self.height, self.height), Image.ANTIALIAS)
 
+        self.photoimage = ImageTk.PhotoImage(self.logoactual)
+        self.logo.configure(image=self.photoimage)
 
+        root.update_idletasks()
 
 if __name__ == '__main__':
     root = tk.Tk()
